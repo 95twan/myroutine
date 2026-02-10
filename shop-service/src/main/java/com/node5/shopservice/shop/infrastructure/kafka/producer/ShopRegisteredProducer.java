66fb015d129e71b4c01426cee86ef0755589311d
김태완 <95twan@naver.com>
2026-02-10 15:19:22 +0900

    feat: 가게 등록시 가게 등록 상태 추가 및 가게 생성 이벤트 발행
    # Conflicts:
    #	config/config/shop-service.yaml
    #	docs/ddl/shop-ddl.sql
    #	shop-service/.gitignore
    #	shop-service/src/main/java/com/node5/shopservice/client/MemberClient.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/application/ShopService.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/domain/ShopRegistration.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/domain/ShopRegistrationRepository.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/domain/ShopRegistrationStatus.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/domain/ShopRepository.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/infrastructure/ShopRegistrationJpaRepository.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/infrastructure/ShopRegistrationRepositoryAdaptor.java
    #	shop-service/src/main/java/com/node5/shopservice/shop/infrastructure/kafka/handler/ShopEventHandler.java

---
+++ shop-service/src/main/java/com/node5/shopservice/shop/infrastructure/kafka/producer/ShopRegisteredProducer.java
@@ -0,0 +1,29 @@
+package com.node5.shopservice.shop.infrastructure.kafka.producer;
+
+import com.node5.common.event.ShopDeletedEvent;
+import com.node5.common.event.ShopRegisteredEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ShopRegisteredProducer {
+
+    private final KafkaTemplate<String, ShopRegisteredEvent> kafkaTemplate;
+
+    @Value("${kafka.topics.shop-registered}")
+    private String topic;
+
+    public void send(ShopRegisteredEvent shopRegisteredEvent) {
+        String key = shopRegisteredEvent.shopId().toString();
+        kafkaTemplate.send(topic, key, shopRegisteredEvent).whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("가게 생성 토픽 발행 실패, key={}", key, ex);
+            }
+        });
+    }
+}
